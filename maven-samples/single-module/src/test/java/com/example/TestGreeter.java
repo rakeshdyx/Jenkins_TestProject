57046b612e06c155e5d311de3443f0d4dea402ad
rakeshdyx <rakeshdyx@gmail.com>
2020-12-08 22:59:59 +0530

    Junit test case added

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -31,9 +31,15 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-    @Test
+  @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+  @Test
+  public void greetShouldIncludeGreetingEndmessage() {
+    String someone = "World, Bye Bye";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
